c1dc4bbad8aff50bb95f5c89318fd17d5f3030f2
Sasha Stepanova <chernishevskie@yandex.ru>
2021-10-21 17:18:10 +0300

    feat: added rate limiter

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const rateLimiter = require('./utils/rateLimiter');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(rateLimiter);
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
