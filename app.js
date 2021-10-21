50be4bf28be8cf4a2fc0e9efcc6166d39eb714fc
Sasha Stepanova <chernishevskie@yandex.ru>
2021-10-21 17:25:57 +0300

    feat: added separate file for the app configuration

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
+const { PORT } = require('./utils/appConfig');
 const rateLimiter = require('./utils/rateLimiter');
-
-// Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(rateLimiter);
---
+++ utils/appConfig.js
@@ -0,0 +1,4 @@
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
+
+module.exports = { PORT };
