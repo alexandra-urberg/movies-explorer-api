4abacc48513e69380420b65b3f193a0583a70ba5
Sasha Stepanova <chernishevskie@yandex.ru>
2021-10-26 02:11:24 +0300

    refactort: changed localhost

--- utils/appConfig.js
+++ utils/appConfig.js
@@ -1,4 +1,4 @@
 // Слушаем 3000 порт
-const { DB = 'mongodb://127.0.0.1:27017/moviesdb', PORT = 3000, JWT_SECRET = 'secret-key' } = process.env;
+const { DB = 'mongodb://localhost:27017/moviesdb', PORT = 3000, JWT_SECRET = 'secret-key' } = process.env;
 
 module.exports = { DB, PORT, JWT_SECRET };
