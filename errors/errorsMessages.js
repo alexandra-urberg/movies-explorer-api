de7daac218b7227a8c86e62cb1a0bfff32debf8b
Sasha Stepanova <chernishevskie@yandex.ru>
2021-10-26 01:59:53 +0300

    refactor: correct version

--- errors/errorsMessages.js
+++ errors/errorsMessages.js
@@ -1,10 +1,13 @@
 // user
-module.exports.emailConflict('The email is already in use');
-module.exports.unfilledEmailAndPassword('Email and password must be filled');
-module.exports.unauthorizedUser('Wrong email or password');
-module.exports.notFoundedUser('The user doesnt exist');
-module.exports.invalidFormat('Invalid id format');
+module.exports.emailConflict = 'The email is already in use';
+module.exports.unfilledEmailAndPassword = 'Email and password must be filled';
+module.exports.unfilledEmailPasswordAndName = 'Name, email and password must be filled';
+module.exports.unauthorizedUser = 'Wrong email or password';
+module.exports.notFoundedUser = 'The user doesnt exist';
+module.exports.invalidFormat = 'Invalid id format';
 // movie
-module.exports.incorrectData('The data is incorrect');
-module.exports.notFoundedMovie('The movie doesnt exist');
-module.exports.forbiddenToDelete('You cant delete this movie');
+module.exports.incorrectData = 'The data is incorrect';
+module.exports.notFoundedMovie = 'The movie doesnt exist';
+module.exports.forbiddenToDelete = 'You cant delete this movie';
+// other
+module.exports.defaultError = 'The adress doesnt exist';
