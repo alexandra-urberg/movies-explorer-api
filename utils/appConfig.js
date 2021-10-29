2e6626706b18894cea84468ff5572a566413df25
Sasha Stepanova <chernishevskie@yandex.ru>
2021-10-29 04:17:25 +0300

    feat: added JWT_SECRET

--- utils/appConfig.js
+++ utils/appConfig.js
@@ -1,4 +1,9 @@
 // Слушаем 3000 порт
-const { DB = 'mongodb://localhost:27017/moviesdb', PORT = 3000, JWT_SECRET = 'secret-key' } = process.env;
+const {
+  DB = 'mongodb://localhost:27017/moviesdb', PORT = 3000, NODE_ENV, JWT_SECRET = '8975SecretCode3q456t',
+} = process.env;
 
-module.exports = { DB, PORT, JWT_SECRET };
+// JWT_SECRET = 'secret-key'
+module.exports = {
+  DB, PORT, JWT_SECRET, NODE_ENV,
+};
