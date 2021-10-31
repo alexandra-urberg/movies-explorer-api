839c8ca0fdc69eb34a618169d722817f1ce41238
Sasha Stepanova <chernishevskie@yandex.ru>
2021-10-31 21:12:05 +0200

    fix: changed secret key after merge

--- utils/appConfig.js
+++ utils/appConfig.js
@@ -1,9 +1,9 @@
 // Слушаем 3000 порт
 const {
-  DB = 'mongodb://localhost:27017/moviesdb', PORT = 3000, NODE_ENV, JWT_SECRET = '8975SecretCode3q456t',
+  DB = 'mongodb://localhost:27017/moviesdb', JWT_SECRET = 'secret-key', PORT = 3000,
 } = process.env;
 
 // JWT_SECRET = 'secret-key'
 module.exports = {
-  DB, PORT, JWT_SECRET, NODE_ENV,
+  DB, PORT, JWT_SECRET,
 };
